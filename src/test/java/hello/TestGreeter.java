10ac6ab6e0b9ed5672413bfc0fa4433d6545c111
cht14 <35692436+cht14@users.noreply.github.com>
2018-03-03 14:17:30 -0600

    Added the passing Test

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -61,5 +61,30 @@ public class TestGreeter {
       assertEquals(g.sayHello(),"Hello World!");
 
    }
+   
+   public void newTest<CT>GreeterPass() 
+
+   {
+
+      g.setName("CharlesTabakForPresident");
+
+      assertEquals(g.getName(),"CharlesTabakForPresident");
+
+      assertEquals(g.sayHello(),"Hello CharlesTabakForPresident!");
+
+   }
+   
+ /*  public void newTest<CT>GreeterFail() 
+
+   {
+
+      g.setName("World");
+
+      assertEquals(g.getName(),"World");
+
+      assertEquals(g.sayHello(),"Hello World!");
+
+   }*/
+   
 
 }
